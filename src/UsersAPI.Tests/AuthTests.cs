1a32d8712299ef9ddbebb2d92fc40fdc4caead05
Leo Campos <leopcampos85@gmail.com>
2024-02-16 18:02:51 -0300

    feat!: Design DDD, criação dos testes.

---
+++ src/UsersAPI.Tests/AuthTests.cs
@@ -0,0 +1,40 @@
+﻿namespace UsersAPI.Tests;
+
+public class AuthTests
+{
+    [Fact(Skip = "Não implementado.")]
+    public void Auth_Post_Login_Returns_Ok()
+    {
+        //TODO
+    }
+
+    [Fact(Skip = "Não implementado.")]
+    public void Auth_Post_Login_Returns_Unathorized()
+    {
+        //TODO
+    }
+
+    [Fact(Skip = "Não implementado.")]
+    public void Auth_Post_Forgot_Password_Returns_Ok()
+    {
+        //TODO
+    }
+
+    [Fact(Skip = "Não implementado.")]
+    public void Auth_Post_Forgot_Password_Returns_BadRequest()
+    {
+        //TODO
+    }
+
+    [Fact(Skip = "Não implementado.")]
+    public void Auth_Post_Reset_Password_Returns_Ok()
+    {
+        //TODO
+    }
+
+    [Fact(Skip = "Não implementado.")]
+    public void Auth_Post_Reset_Password_Returns_BadRequest()
+    {
+        //TODO
+    }
+}
