5392d30b2086b9659241b8a3e8b3073ab68b745b
Leo Campos <leopcampos85@gmail.com>
2024-02-18 20:53:41 -0300

    Feat: Configuração Cors, Models, Repositories e início da implantação da camada Infra.Data.

---
+++ src/UsersAPI.Domain/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace UsersAPI.Domain.Interfaces.Repositories;
+
+public interface IUnitOfWork : IDisposable
+{
+    IUserRepository UserRepository { get; }
+    void SaveChanges();
+}
---
+++ src/UsersAPI.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,7 @@
+﻿using UsersAPI.Domain.Models;
+
+namespace UsersAPI.Domain.Interfaces.Repositories;
+
+public interface IUserRepository : IBaseRepository<User, Guid>
+{
+}
