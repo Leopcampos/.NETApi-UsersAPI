ccb1d92d34dd345b74306bbcfabe583a7b61692e
Leo Campos <leopcampos85@gmail.com>
2024-02-24 19:56:10 -0300

    feat: Criando a interface, VO e alterando a DomainService para enviar email quando a conta do usuário for criada.

--- src/UsersAPI.Domain/Services/UserDomainService.cs
+++ src/UsersAPI.Domain/Services/UserDomainService.cs
@@ -1,24 +1,39 @@
 ﻿using UsersAPI.Domain.Exceptions;
+using UsersAPI.Domain.Interfaces.Messages;
 using UsersAPI.Domain.Interfaces.Repositories;
 using UsersAPI.Domain.Interfaces.Services;
 using UsersAPI.Domain.Models;
+using UsersAPI.Domain.ValueObjects;
 
 namespace UsersAPI.Domain.Services;
 
 public class UserDomainService : IUserDomainService
 {
     private readonly IUnitOfWork? _unitOfWork;
+    private readonly IUserMessageProducer? _userMessageProducer;
 
-    public UserDomainService(IUnitOfWork? unitOfWork)
-        => _unitOfWork = unitOfWork;
+    public UserDomainService(IUnitOfWork? unitOfWork, IUserMessageProducer? userMessageProducer)
+    {
+        _unitOfWork = unitOfWork;
+        _userMessageProducer = userMessageProducer;
+    }
 
     public void Add(User user)
     {
-        if(Get(user.Email) != null)
+        if (Get(user.Email) != null)
             throw new EmailAlreadyExistsException(user.Email);
 
         _unitOfWork?.UserRepository.Add(user);
         _unitOfWork?.SaveChanges();
+
+        _userMessageProducer?.Send(new UserMessageVO
+        {
+            Id = user.Id,
+            SendedAt = DateTime.Now,
+            To = user.Email,
+            Subject = "Parabéns, sua conta de usuário foi criada com sucesso!",
+            Body = @$"Olá {user.Name}, seu cadastro foi realizado com sucesso em nosso sistema."
+        });
     }
 
     public void Update(User user)
