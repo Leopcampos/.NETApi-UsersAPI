34c66d2553d7a3366b01c535c47e7153c49e8fea
Leo Campos <leopcampos85@gmail.com>
2024-02-25 16:27:39 -0300

    feat: Criando e configurando a camada de infra.Message para o RabbitMQ.

---
+++ src/UsersAPI.Infra.Messages/Producers/UserMessageProducer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+using UsersAPI.Domain.Interfaces.Messages;
+using UsersAPI.Domain.ValueObjects;
+using UsersAPI.Infra.Messages.Settings;
+
+namespace UsersAPI.Infra.Messages.Producers;
+
+public class UserMessageProducer : IUserMessageProducer
+{
+    private readonly RabbitMQSettings? _rabbitMQSettings;
+    public UserMessageProducer(IOptions<RabbitMQSettings?> rabbitMQSettings)
+        => _rabbitMQSettings = rabbitMQSettings.Value;
+
+    public void Send(UserMessageVO userMessage)
+    {
+        //Conectando no servidor do RabbitMQ
+        var connectionFactory = new ConnectionFactory() { Uri = new Uri(_rabbitMQSettings?.Url) };
+
+        using (var connection = connectionFactory.CreateConnection())
+        {
+            using (var model = connection.CreateModel())
+            {
+                //Criando a fila
+                model.QueueDeclare(
+                    queue: _rabbitMQSettings.Queue, //Nome da fila
+                    durable: true, //Não vai apagar as filas ao desligar ou reiniciar o broker
+                    autoDelete: false,//apagar ou não a fila quando estiver sem mensagens (vazia)
+                    exclusive: false, //fila exclusiva para uma unica aplicaçao ou não
+                    arguments: null
+                    );
+
+                //Gravando mensagem na fila
+                model.BasicPublish(
+                    exchange: string.Empty,
+                    routingKey: _rabbitMQSettings.Queue,
+                    basicProperties: null,
+                    body: Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(userMessage))
+                    );
+            }
+        }
+    }
+}
---
+++ src/UsersAPI.Infra.Messages/Settings/RabbitMQSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace UsersAPI.Infra.Messages.Settings;
+
+public class RabbitMQSettings
+{
+    public string? Url { get; set; }
+    public string? Queue { get; set; }
+}
