1b9000d5fe091f01c808b74f07324f75965be31e
Leo Campos <leopcampos85@gmail.com>
2024-02-26 19:50:09 -0300

    feat: Consumindo a api para fazer envio de emails quando o usuário criar seu usuário.

---
+++ src/UsersAPI.Infra.Messages/Models/MessagesRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UsersAPI.Infra.Messages.Models;
+
+public class MessagesRequestModel
+{
+    public string? MailTo { get; set; }
+    public string? Subject { get; set; }
+    public string? Body { get; set; }
+    public bool IsBodyHtml { get; set; }
+}
